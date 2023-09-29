eeec7bd16a058887bb4b5fb443a523a00870f9c3
Matheus Sousa <dsousamt@gmail.com>
2023-09-29 12:48:01 -0300

    Atualiza o endpoint

--- src/main/java/com/example/blog/controller/PostagemController.java
+++ src/main/java/com/example/blog/controller/PostagemController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/postagem")
+@RequestMapping("/postagens")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PostagemController {
 
